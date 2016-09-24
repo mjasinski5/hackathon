ec370d6427ab5face082b8ad001c1313b3fb7312
kiwanska <sobylmine@gmail.com>
2016-09-24 18:32:26 +0200

    dla Kuby

--- boilerplate-design/src/components/Main.js
+++ boilerplate-design/src/components/Main.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import {Doughnut} from 'react-chartjs-2';
+
+const state = ({
+  labels: [
+    'Red',
+    'Green',
+    'Yellow'
+  ],
+  datasets: [{
+    data: [450, 123, 67],
+    backgroundColor: [
+    '#CCC',
+    '#36A2EB',
+    '#FFCE56'
+    ],
+    hoverBackgroundColor: [
+    '#FF6384',
+    '#36A2EB',
+    '#FFCE56'
+    ],
+    borderColor: [
+    '#e6e6e6',
+    '#e6e6e6',
+    '#e6e6e6'
+    ],
+    hoverBorderColor: [
+    '#e6e6e6',
+    '#e6e6e6',
+    '#e6e6e6'
+    ]
+  }]
+});
 
 export default function Main(){
   return(
     <div>
-      Main
+      <Doughnut data={state} />
     </div>
   );
 }
