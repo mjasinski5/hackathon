e3d45e66d5c02ced2a4bfad481f7a3678f492ba3
Gawlas, Jakub <jakub.gawlas@sap.com>
2016-09-24 16:55:26 +0200

    society weights

--- react-mobx/src/components/RightAside/index.js
+++ react-mobx/src/components/RightAside/index.js
@@ -6,7 +6,7 @@ function RightAside({ store }){
     console.log('chartDataDoughnut', store.chartDataDoughnut)
   return(
     <aside className='right'>
-
+      <p>{store.societySatisfaction}</p>
       <div style={{width: '800px'}}>
         <Doughnut 
           data={store.chartDataDoughnut} 
