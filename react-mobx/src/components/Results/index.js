c9d6a3ea515a0cbd404eaf5b9816cc876f284ef5
kiwanska <sobylmine@gmail.com>
2016-09-25 14:31:14 +0200

    results view

--- react-mobx/src/components/Results/index.js
+++ react-mobx/src/components/Results/index.js
@@ -27,15 +27,34 @@ export default class Results extends Component {
         <h2>Twój wynik:</h2>
         <div>
           <div className="profile-pic"></div>
-          <div>
+          <div className="data">
             <h1 className="name">Jan, 55 lat</h1>
+            <p>
+              <p>priorytety</p>
+              <p>BEZPIECZEŃSTWO</p>
+              <p>KULTURA</p>
+              <p>POMOC SPOŁECZNA</p>
+            </p>
+            <p>
+              <p>wydatki</p>
+              <p>323 mln</p>
+            </p>
+            <p>
+              <p>deficyt</p>
+              <p>123 mln</p>
+            </p>
+            <p>
+              <p>zadowolenie społeczeństwa</p>
+              <p>55%</p>
+            </p>
           </div>
-          <div></div>
-          <h2>masz <span className="">40%</span> szans na wygraną w wyborach!</h2>
+          <div className="graph"></div>
         </div>
-        <Link onClick={this.saveForm.bind(this)} to='app'>
-          zagraj!
-        </Link>
+        <div>
+         <h1 className="final-result">masz <span >40%</span> szans na wygraną w wyborach!</h1>
+         <h3>Twoją największą grupą wyborczą są: <span>EMERYCI</span></h3>
+        </div>
+       
       </section> 
     );
   }
