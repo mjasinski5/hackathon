41fbebb9276286adfe361b6195362ed2f347773c
Jasinski, Mariusz <mariusz.jasinski@sap.com>
2016-09-25 13:50:07 +0200

    xd

--- react-mobx/src/model/notifications.js
+++ react-mobx/src/model/notifications.js
@@ -14,5 +14,10 @@ export default [
     filter: (store) => store.getOswiataValue < 870,
     message: 'UWAŻAJ! Zbyt duże obcięcie wydatków na edukację sprawi, że będziesz musiał zamknąć trzy szkoły lub 1000 dzieci nie będzie mogło w przyszłym roku skorzystać z bezpłatnych obiadów w szkole. Postępuj ostrożnie, bo możesz rozgniewać swoich wyborców.',
     level: 'warning'
+  },
+  {
+    filter: (store) => store.currentLoanStateInPercent > 100,
+    message: 'Przekroczyłeś indywidualny wskaźnik zadłużenia! Dowiedz się więcej.',
+    level: 'warning'
   }
 ];
