439df9aad6f11c5b283a8e094ab0fea7f8eacdc7
Jasinski, Mariusz <mariusz.jasinski@sap.com>
2016-09-27 08:57:53 +0200

    xd

--- react-mobx/config/webpack.config.prod.js
+++ react-mobx/config/webpack.config.prod.js
@@ -83,8 +83,10 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+      'components': path.join(paths.appSrc, 'components'),
       'commons': path.join(paths.appSrc, 'commons'),
-      'components': path.join(paths.appSrc, 'components')
+      'data': path.join(paths.appSrc, 'data'),
+      'model': path.join(paths.appSrc, 'model')
     }
   },
   
